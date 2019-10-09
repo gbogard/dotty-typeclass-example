c566b954927826efb3e31e030c4fe80a62ce3133
Guillaume Bogard <bogardguillaume@gmail.com>
2019-10-09 17:36:31 +0200

    Improved example

--- build.sbt
+++ build.sbt
@@ -3,10 +3,7 @@ val dottyVersion = "0.19.0-RC1"
 lazy val root = project
   .in(file("."))
   .settings(
-    name := "dotty-simple",
+    name := "dotty-typeclass-example",
     version := "0.1.0",
-
     scalaVersion := dottyVersion,
-
-    libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test"
   )
--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -11,27 +11,16 @@ import Inversible.given
 
 object Main {
 
+  // We can use a context bound to tell the compiler that the `A` expect must be a member of the Inversible type class
+  // Then we can use our extension method without the need for an implicit class
+  def printInverse[A: Inversible](input: A): Unit = println(s"The inverse of $input is ${input.inverse}")
+
   def main(args: Array[String]): Unit = {
     val r = Ratio(10, 45)
     val s = "Hello World!"
-
-   // Then we can use our `reverse` extension method using an infix dot notation,
-   // without the need for implicit classes
-
-    val reversedR = r.inverse
-
-    val reversedS = s.inverse
-
-
-
-    println(
-      s"""
-      | String : $s
-      | Ratio : $r
-      | Reversed String : $reversedS
-      | Reversed Ratio : $reversedR
-      """.stripMargin
-    )
+    
+    printInverse(s)
+    printInverse(r)
   }
 
 }
